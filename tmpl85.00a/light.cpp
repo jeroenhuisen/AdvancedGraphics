061eef8e9ea4deff013c2a4a07131677a18013fa
jeroenhuisen <jeroenhuisen@outlook.com>
2016-11-26 11:47:20 +0100

    stage last things laptop

--- tmpl85.00a/light.cpp
+++ tmpl85.00a/light.cpp
@@ -5,4 +5,5 @@
 Light::Light(glm::vec3 position, glm::vec3 direction, float lightIntensity, Tmpl8::Pixel color) :
 	Placeable(position, direction), lightIntensity(lightIntensity), color(color) {
 
+
 }
