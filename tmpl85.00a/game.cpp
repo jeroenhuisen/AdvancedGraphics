e22cdd29c8f0ecd155865794d0ef2d0ce3c0655d
jeroenhuisen <jeroenhuisen@outlook.com>
2016-12-01 16:34:55 +0100

    Looks nice and stuff cant remember if I really changed something

--- tmpl85.00a/game.cpp
+++ tmpl85.00a/game.cpp
@@ -17,16 +17,22 @@ void Game::Init()
 
 
 	Material* red = new Material(Color(0xFF,0,0));
-	Material* green = new Material(Color(0,0x1F,0));
+	Material* green = new Material(Color(0,0xFF,0));
 	Material* redish = new Material(Color(0x1F, 0, 0));
 	Sphere* sphere = new Sphere(glm::vec3(10.0f, 10.0f, 10.0f), 2.0f, red);
 	Plane* plane = new Plane(glm::vec3(0, 1108, 0), glm::vec3(0, 1, 0), 100, 100, redish);
 	Plane* plane1 = new Plane(glm::vec3(500, 1108, 400),  glm::vec3(0, 0, 1), 100, 100, red);
 	PointLight* pointLight = new PointLight(glm::vec3(0, 0, 0), 5000, Color(0xFF,0xFF,0xFF), 1.0f, 0.007f, 0.002f);
-	PointLight* pointLightSmall = new PointLight(glm::vec3(70, 1100, 0), 1000, Color(0xFF,0xFF,0xFF), 1.0f, 0.2f, 0.05f); //distance is 8 on closes point I/(1.0+0.7*8+0.5*8^2)
-	scene.addLight(pointLight);
+	PointLight* pointLightSmall = new PointLight(glm::vec3(70, 1100, 0), 1000, Color(0x00,0x00,0x00), 1.0f, 0.2f, 0.05f); 
+	PointLight* pointLightSmall1 = new PointLight(glm::vec3(200, 1100, 0), 1000, Color(0xFF, 0x00, 0x00), 1.0f, 0.2f, 0.05f);
+	PointLight* pointLightSmall2 = new PointLight(glm::vec3(-30, 1100, -250), 1000, Color(0x00, 0xFF, 0x00), 1.0f, 0.2f, 0.05f);
+	PointLight* pointLightSmall3 = new PointLight(glm::vec3(-265, 1100, 0), 1000, Color(0x00, 0xFF, 0xFF), 1.0f, 0.2f, 0.05f);
+//	scene.addLight(pointLight);
 	scene.addLight(pointLightSmall);
-//	scene.addObject(plane);
+	scene.addLight(pointLightSmall1);
+	scene.addLight(pointLightSmall2);
+	scene.addLight(pointLightSmall3);
+	//scene.addObject(plane);
 	scene.addObject(plane1);
 
 	Material* gray = new Material(Color(0xD3, 0xD3, 0xD3));
