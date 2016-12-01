ced78c3a8e19ec618645848b5dd1f25977f24a8d
Mercury Dev <Mercury Dev@LAPTOP-VDIE9G3G>
2016-12-01 19:49:34 +0100

    Added a sphere class 420 blaze it

--- tmpl85.00a/sphere.cpp
+++ tmpl85.00a/sphere.cpp
@@ -5,3 +5,15 @@ Sphere::Sphere(glm::vec3 position, float radius, Material* material) :
 	Object(position, vec3(0, 0, 0), material), radius(radius) {
 
 }
+
+glm::vec3 Sphere::intersection(Ray r, float* distance) {
+	glm::vec3 c = position - r.getOrigin();
+	float t = glm::dot(c, glm::normalize(r.getDirection()));
+	vec3 q = c - t * glm::normalize(r.getDirection());
+	float p2 = glm::dot(q, q);
+	if ((p2 > radius) && (t > 0)) {
+		return glm::vec3();
+	}
+	*distance = glm::length(c) - radius;
+	return r.getOrigin() + *distance * c;
+}
