c753996020929e80c80440502d65d2cbbc9b67b7
Mercury Dev <Mercury Dev@LAPTOP-VDIE9G3G>
2016-12-03 19:19:35 +0100

    Implemented triangles

--- tmpl85.00a/game.cpp
+++ tmpl85.00a/game.cpp
@@ -26,19 +26,20 @@ void Game::Init()
 	Sphere* sphereB = new Sphere(glm::vec3(-300.0f, 1100.0f, 200.0f), 100.0f, blueish);
 	Plane* plane = new Plane(glm::vec3(0, 1108, 0), glm::vec3(0, 1, 0), 100, 100, redish);
 	Plane* plane1 = new Plane(glm::vec3(500, 1108, 400),  glm::vec3(0, 0, 1), 100, 100, red);
-	PointLight* pointLight = new PointLight(glm::vec3(0, 0, 0), 5000, Color(0xFF,0xFF,0xFF), 1.0f, 0.007f, 0.002f);
-	PointLight* pointLightSmall = new PointLight(glm::vec3(70, 1100, 0), 1000, Color(0x00,0x00,0x00), 1.0f, 0.2f, 0.05f); 
+	Triangle* triangle = new Triangle(glm::vec3(0, 900, 0), glm::vec3(200, 1000, 0), glm::vec3(100, 900, 100), redish);
+	PointLight* pointLight = new PointLight(glm::vec3(0, 0, -200), 2000, Color(0xFF,0xFF,0xFF), 1.0f, 0.007f, 0.002f);
+	PointLight* pointLightSmall = new PointLight(glm::vec3(70, 1100, 0), 10000, Color(0x00,0x00,0x00), 1.0f, 0.2f, 0.05f); 
 	PointLight* pointLightSmall1 = new PointLight(glm::vec3(200, 1100, 0), 1000, Color(0xFF, 0x00, 0x00), 1.0f, 0.2f, 0.05f);
 	PointLight* pointLightSmall2 = new PointLight(glm::vec3(-30, 1100, -250), 1000, Color(0x00, 0xFF, 0x00), 1.0f, 0.2f, 0.05f);
 	PointLight* pointLightSmall3 = new PointLight(glm::vec3(-265, 1100, 0), 1000, Color(0x00, 0xFF, 0xFF), 1.0f, 0.2f, 0.05f);
-	//scene.addLight(pointLight);
+	scene.addLight(pointLight);
 	scene.addLight(pointLightSmall);
 	scene.addLight(pointLightSmall1);
 	scene.addLight(pointLightSmall2);
 	scene.addLight(pointLightSmall3);
 								//scene.addObject(plane);
 	scene.addObject(plane1);
-
+	scene.addObject(triangle);
 	scene.addObject(sphereR);
 	scene.addObject(sphereG);
 	scene.addObject(sphereB);
---
+++ tmpl85.00a/triangle.cpp
@@ -0,0 +1,34 @@
+#include "template.h"
+#include "triangle.h"
+
+
+Triangle::Triangle(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3, Material* material) :
+	Object(v1, glm::normalize(glm::cross((v2-v1),(v3-v1))), material), v1(v1), v2(v2), v3(v3){
+
+}
+
+glm::vec3 Triangle::intersection(Ray r, float* distance) {
+	glm::vec3 e1 = v2 - v1;
+	glm::vec3 e2 = v3 - v1;
+	glm::vec3 P = glm::cross(r.getDirection(), e2);
+	float det = glm::dot(e1, P);
+	if ((det > -EPSILON) && (det < EPSILON)) {
+		return glm::vec3(); // Ray is either parallel to or inside the triangle
+	}
+	float inv_det = 1 / det;
+	glm::vec3 T = r.getOrigin() - v1;
+	float u = glm::dot(T, P) * inv_det;
+	if ((u < 0) || (u > 1)) {
+		return glm::vec3(); // Intersection lies outside of the triangle
+	}
+	glm::vec3 Q = glm::cross(T, e1);
+	float v = dot(r.getDirection(), Q) * inv_det;
+	if ((v < 0) || (u + v > 1)) {
+		return glm::vec3(); // Again, intersection outside of the triangle
+	}
+	float t = glm::dot(e2, Q) * inv_det;
+	if (t > 0) {
+		*distance = glm::min(*distance, t);
+	}
+	return direction; // Normalised normal of the triangle
+}
