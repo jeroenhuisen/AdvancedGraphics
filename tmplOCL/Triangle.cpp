ff1256e47b6dc073bc3d6a77af9e681e82542cc1
JeroenHuisen <jeroenhuisen@hotmail.com>
2017-01-26 23:21:31 +0100

    don't use precalculated normals since they cause issues (probably because the normals are bit off (for smoothing?) and that can cause wrong intersections"

--- tmplOCL/Triangle.cpp
+++ tmplOCL/Triangle.cpp
@@ -25,7 +25,8 @@ Triangle createTriangle(vec3 v1, vec3 v2, vec3 v3, vec3 n1, vec3 n2, vec3 n3, Ma
 	t.v2 = { v2.x, v2.y, v2.z };
 	t.v3 = { v3.x, v3.y, v3.z };
 
-	vec3 dir = normalize(n1 + n2 + n3);
+	//vec3 dir1 = normalize(n1 + n2 + n3);//doesn't work
+	vec3 dir = normalize(cross((v2 - v1), (v3 - v1)));
 	t.direction = { dir.x, dir.y, dir.z };
 
 	vec3 e1 = v2 - v1;
